d2ca7d9d53f1b62da5eeddf777bff906a735d2c1
THESHREDDER\TheBeast <sharad@unknownworlds.com>
2022-01-29 16:38:43 -0500

    reverting attempt 1 just to make sure

--- tests/example.rs
+++ tests/example.rs
@@ -53,7 +53,7 @@ mod example {
 
     // Steps are defined with `given`, `when` and `then` attributes.
     #[given(regex = r"^a (hungry|satiated) cat$")]
-    fn hungry_cat(world: &mut AnimalWorld, state: String) {
+    async fn hungry_cat(world: &mut AnimalWorld, state: String) {
         match state.as_str() {
             "hungry" => world.cat.fullness = 0,
             "satiated" => world.cat.fullness = 1,
@@ -70,12 +70,12 @@ mod example {
     // }
 
     #[when(regex = r"^I feed the cat (\d+) times$")]
-    fn feed_cat(world: &mut AnimalWorld, count: u32) {
+    async fn feed_cat(world: &mut AnimalWorld, count: u32) {
         world.cat.feed(count);
     }
 
     #[then(regex = r"^the cat (is not hungry|has exploded)?$")]
-    fn cat_is_fed(world: &mut AnimalWorld, result: String) {
+    async fn cat_is_fed(world: &mut AnimalWorld, result: String) {
         match result.as_str() {
             "is not hungry" => assert!(world.cat.is_full()),
             "has exploded" => assert!(world.cat.has_exploded()),
