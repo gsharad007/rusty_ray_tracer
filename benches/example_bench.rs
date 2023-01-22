f785c3e4cab68b7a784256d8c94f5f09c1ee80c2
THESHREDDER\TheBeast <gsharad007@hotmail.com>
2023-01-21 20:34:36 -0500

    Adding iai and criterion bench libs

---
+++ benches/example_bench.rs
@@ -0,0 +1,48 @@
+#![feature(test)]
+
+extern crate test;
+use test::black_box;
+
+pub mod helpers;
+
+fn fibonacci_recursive(n: u64) -> u64 {
+    match n {
+        0 => 1,
+        1 => 1,
+        n => fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2),
+    }
+}
+
+fn fibonacci_iterative(n: u64) -> u64 {
+    let mut a = 0;
+    let mut b = 1;
+
+    match n {
+        0 => b,
+        _ => {
+            for _ in 0..n {
+                let c = a + b;
+                a = b;
+                b = c;
+            }
+            b
+        }
+    }
+}
+
+fn fib_rec() -> u64 {
+    fibonacci_recursive(black_box(20))
+}
+
+fn fib_iter() -> u64 {
+    fibonacci_iterative(black_box(20))
+}
+
+crit_group!(benches, fib_rec, fib_iter);
+crit_main!(benches);
+iai_main!(fib_rec, fib_iter);
+fn main() {
+    iai_runner();
+
+    crit_runner();
+}
---
+++ benches/helpers/mod.rs
@@ -0,0 +1,2 @@
+pub mod iai_macros;
+pub mod crit_macros;
